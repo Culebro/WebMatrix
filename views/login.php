1b61a78d06cd5780228612a49104b2c22c0c8027
Carlos Culebro <cculebroes@gmail.com>
2018-06-18 20:09:36 -0500

    -

---
+++ views/login.php
@@ -0,0 +1,19 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="/Matrix/WebMatrix/assets/js/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8"></div>
+                    <div class="col-md-8"></div>
+                </div>
+            </div>
+    </body>
+</html>
