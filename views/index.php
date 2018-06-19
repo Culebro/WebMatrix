25e3cc84ddc9996bf41bba569a68e41ee27fca1a
Carlos Culebro <cculebroes@gmail.com>
2018-06-19 08:59:36 -0500

    contenido innecesario

--- views/index.php
+++ views/index.php
@@ -1,8 +1,3 @@
-<?php
-        $templateManager = new League\Plates\Engine('views');
-        echo $templateManager->render('index', array('titulo' => 'Principal'))
-?>
-<?php  $this->layout('template/index')?>
 
 
 
